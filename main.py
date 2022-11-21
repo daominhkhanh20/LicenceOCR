702f6b4c2a2760d1f14c3b630acfd59a60c9673e
daominhkhanh20 <khanhc1k36@gmail.com>
2022-11-21 09:26:02 +0700

    add

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from PIL import Image
 import numpy as np
 import cv2
 import io
-from model import detector
+from model import licence_plate
 import random 
 from io import BytesIO
 
@@ -25,10 +25,8 @@ async def root():
 @app.post('/upload')
 async def receive_file(file: UploadFile = File(...)):
     image = read_imagefile(await file.read())
-    print(image.shape)
-    boxs = detector(image)[0, :]
-    print(boxs[0], boxs[1],boxs[2], boxs[3])
+    plate, boxs = licence_plate(image)
     cv2.rectangle(image, (int(boxs[0]), int(boxs[1])), (int(boxs[2]), int(boxs[3])), (255, 0, 0), 2)
-    print(boxs)
+    cv2.putText(image, plate, (int(boxs[0] - 5), int(boxs[1] - 5)),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 172, 5), 2)
     res, img_png = cv2.imencode('.png', image)
     return StreamingResponse(io.BytesIO(img_png.tobytes()), media_type='image/png')
--- model.py
+++ model.py
@@ -1,7 +1,19 @@
 from pytorchyolo import models, detect
 import torch
 import numpy as np 
+import torch
 from PIL import Image 
+from matplotlib import pyplot as plt
+from vietocr.tool.predictor import Predictor
+from vietocr.tool.config import Cfg
+
+config = Cfg.load_config_from_name('vgg_transformer')
+config['weights'] = 'ocr/weights/transformerocr.pth'
+config['cnn']['pretrained']=False
+config['vocab'] = 'QWERTYUIOPASDFGHJKLZXCVBNM0123456789-.'
+config['device'] = torch.device("cpu")
+config['predictor']['beamsearch']=False
+model_detector = Predictor(config)
 
 device = torch.device('cuda' if torch.cuda.is_available() else "cpu")
 model = models.load_model('darknet/cfg/yolov3-tiny.cfg', 'darknet/backup/yolov3-tiny_2000.weights').to(device)
@@ -12,11 +24,13 @@ def detector(image):
     return boxes
 
 def ocr(image):
-    pass 
+    plate = model_detector.predict(image)
+    return plate
 
 def licence_plate(image):
     boxes = detector(image)
-    x,y,x1,y1 = boxes[0][0], boxes[0][1], boxes[0][2], boxes[0][3]
-    numpy_image = image[x:x1, y:y1,:]
+    x,y,x1,y1 = int(boxes[0][0]), int(boxes[0][1]), int(boxes[0][2]), int(boxes[0][3])
+    numpy_image = image[y:y1, x:x1,:]
     imgage_ocr = Image.fromarray(np.uint8(numpy_image)).convert('RGB')
     plate = ocr(image=imgage_ocr)
+    return plate, boxes[0]
