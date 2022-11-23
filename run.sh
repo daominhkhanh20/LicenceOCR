f65221269119521526925e6acf5742284f5ecfb3
daominhkhanh20 <khanhc1k36@gmail.com>
2022-11-23 21:32:22 +0700

    add

---
+++ run.sh
@@ -0,0 +1,8 @@
+mkdir models 
+mv Model* models
+export KAGGLE_USERNAME="daominhkhanh"
+export KAGGLE_KEY="aad7a721ee47d7c7dec658896bcefedd"
+kaggle datasets init -p models
+sed -i 's/INSERT_TITLE_HERE/FbModel2311/g' model/dataset-metadata.json
+sed -i 's/INSERT_SLUG_HERE/FbModel2311/g' model/dataset-metadata.json
+kaggle datasets create -p ocr --dir-mode zip
