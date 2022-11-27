bde82c03783ba6e9a0d1d071ea76592bd0cb4cda
daominhkhanh20 <khanhc1k36@gmail.com>
2022-11-27 19:50:28 +0700

    add

--- vietocr/model/backbone/cnn.py
+++ vietocr/model/backbone/cnn.py
@@ -15,14 +15,9 @@ class CNN(nn.Module):
         elif backbone == 'resnet50':
             self.model = Resnet50(**kwargs)
 
+        for param in self.model.features.parameters():
+            param.requires_grad = True
+
     def forward(self, x):
         return self.model(x)
 
-    def freeze(self):
-        for name, param in self.model.features.named_parameters():
-            if name != 'last_conv_1x1':
-                param.requires_grad = False
-
-    def unfreeze(self):
-        for param in self.model.features.parameters():
-            param.requires_grad = True
