d15cb27521330366e59390f0fe37f43a3a8bf604
daominhkhanh20 <khanhc1k36@gmail.com>
2022-11-29 09:05:24 +0700

    add

--- vietocr/tool/translate.py
+++ vietocr/tool/translate.py
@@ -118,7 +118,7 @@ def translate(img, model, max_seq_length=128, sos_token=1, eos_token=2):
 
 
 def build_model(config):
-    vocab = Vocab(config['vocab'], config['dataset']['train_annotation'])
+    vocab = Vocab(config['vocab'], path_train = f"{config['dataset']['data_root']}/{config['dataset']['train_annotation']}")
     device = config['device']
     
     model = VietOCR(len(vocab),
